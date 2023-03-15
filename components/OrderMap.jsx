449af7a7f158bef3500577296200f7928ac31cff
Suraj S <ksuraj626@gmail.com>
2023-03-15 22:57:19 +0530

    map icon rectified

--- components/OrderMap.jsx
+++ components/OrderMap.jsx
@@ -1,10 +1,5 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 // import 'leaflet/dist/leaflet.css'
-// import markerIconPng from "leaflet/dist/images/marker-icon.png"  //
-// import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-// import {Icon} from 'leaflet'
-
-// import { MapContainer, TileLayer, Marker, Popup } from 'https://cdn.esm.sh/react-leaflet'
 
 export default function OrderMap() {
 
@@ -16,7 +11,6 @@ export default function OrderMap() {
     
     <MapContainer center={position} zoom={12} scrollWheelZoom={true} style={{ height: '40vh', width: "50vw", }}>
       <TileLayer
-        // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       
@@ -27,5 +21,3 @@ export default function OrderMap() {
     </MapContainer>
   );
 }
-
-//icon={new Icon({iconRetinaUrl: markerIconPng, iconUrl: markerIconPng, iconSize: [25,41], shadowUrl: iconShadow, iconAnchor: [12,41]})}
