5912ad2f470e1ea5ad105109ccd14a390bb81e83
Karthik Raju <karthikraju391@gmail.com>
2023-06-02 13:45:30 +0530

    update location api

--- pages/api/location.js
+++ pages/api/location.js
@@ -10,6 +10,37 @@ export default async function handle(req, res) {
 
 			const { lat, long, sp, id } = jsonData;
 
+			if (
+				lat === "0" ||
+				lat === "0.00" ||
+				long === "0" ||
+				long === "0.00" ||
+				sp === "0" ||
+				sp === "0.00"
+			) {
+				return res.status(400).json({ error: "Missing required fields" });
+			}
+
+			const existingLocation = await prisma.location.findFirst({
+				where: {
+					deviceId: id,
+				},
+			});
+
+			if (existingLocation) {
+				const location = await prisma.location.update({
+					where: {
+						id: existingLocation.id,
+					},
+					data: {
+						speed: parseFloat(sp),
+						latitude: parseFloat(lat),
+						longitude: parseFloat(long),
+					},
+				});
+				return res.json({ location });
+			}
+
 			const location = await prisma.location.create({
 				data: {
 					deviceId: id,
