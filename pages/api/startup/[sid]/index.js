c3f6623e15922e2c65c43aec0c270035d9ec0bcb
Karthik Raju <karthikraju391@gmail.com>
2023-06-03 08:09:10 +0530

    complete merge

--- pages/api/startup/[sid]/index.js
+++ pages/api/startup/[sid]/index.js
@@ -1,6 +1,6 @@
 import prisma from "../../../../lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]";
+import { authOptions } from "../../auth/[...nextauth]";
 
 export default async function handle(req, res) {
 	const session = await getServerSession(req, res, authOptions);
